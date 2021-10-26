09b2c312a527f6d95ac29d3298a7301478d59e34
1816357 <1816357@brunel.ac.uk>
2021-10-26 13:42:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# myList = [9, "salah", [5,6]]
+# newList = myList + [23, 24]
+# newList.append("mohamed")
+# newList.insert(2, 800)
+# newList.insert(4,"ahmed")
+# newList.extend([6,6,6])
+# newList.extend (["hello"])
+# # del newList[2]
+# # del newList[1]
+# newList.remove("hello")
+# print(newList)
+# # del newList [1:3:2]
+# firstItem = newList[0]
+# print(firstItem)
+# newList [1] = "SALLAH"
+# print(newList)
+
+
+# print(user_input)
+
+myList = ["sword", "gun"]
+user_input = input("What do yuo want?: ")
+user_input = user_input.lower()
+if user_input in myList:
+    print ("that's it")
+
